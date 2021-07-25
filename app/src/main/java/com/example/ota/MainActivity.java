e32eedd3d45a549af9c5a40bcaec44a9125e9827
DavidNyx <david.quoccuong2001@gmail.com>
2021-07-25 18:05:52 +0700

    login screen

--- app/src/main/java/com/example/ota/MainActivity.java
+++ app/src/main/java/com/example/ota/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.ota;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickLogin(View view){
+
+        EditText editUsername = findViewById(R.id.editUsername);
+        EditText editPassword = findViewById(R.id.editPassword);
+        if (editUsername.getText().toString().equals(""))
+        {
+            System.out.println("empty");
+        }
+        else
+            System.out.println(editUsername.getText().toString());
+        if (editPassword.getText().toString().equals(""))
+        {
+            System.out.println("empty");
+        }
+        else
+            System.out.println(editPassword.getText().toString());
+        Toast.makeText(this, "Login successfully", Toast.LENGTH_SHORT).show();
     }
 }
