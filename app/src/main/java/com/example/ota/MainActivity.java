a15cc780543f5d6a188d99489940ff5e47af2304
DNX2461 <david.quoccuong2001@gmail.com>
2021-07-30 14:37:26 +0700

    add empty screen

--- app/src/main/java/com/example/ota/MainActivity.java
+++ app/src/main/java/com/example/ota/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Login successfully", Toast.LENGTH_SHORT).show();
         }
 
+        if (editUsername.getText().toString().equals("student"))
+            startActivity(new Intent(MainActivity.this, StudentActivity.class));
+
+        if (editUsername.getText().toString().equals("parents"))
+            startActivity(new Intent(MainActivity.this, StudentActivity.class));
     }
 
     public void onClickForgotPassword(View view){
