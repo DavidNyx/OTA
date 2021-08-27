c0044efd4a7f61a181047c219e7463302fe82a6a
dat-badboy <64117485+dat-badboy@users.noreply.github.com>
2021-08-27 23:50:45 +0700

    abcxyz

--- app/src/main/java/com/example/ota/MainActivity.java
+++ app/src/main/java/com/example/ota/MainActivity.java
@@ -11,6 +11,8 @@ import android.widget.Toast;
 import android.content.Intent;
 import android.app.Fragment;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -20,8 +22,12 @@ import com.android.volley.toolbox.StringRequest;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
     private String url = "https://ota-be-server.herokuapp.com/linkserver/account/login";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,36 +48,42 @@ public class MainActivity extends AppCompatActivity {
             String Username = editUsername.getText().toString();
             String Password = editPassword.getText().toString();
             JSONObject data = null;
-            try {
-                //String datas = "{'ID':"+Username+",'password':"+Password+"}";
-                data = new JSONObject();
+            data = new JSONObject();
+            try { // day la data truyen cho server
                 data.put("ID",Username);
                 data.put("password",Password);
                 Toast.makeText(MainActivity.this, data.toString(), Toast.LENGTH_SHORT).show();
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-            final String url = "https://ota-be-server.herokuapp.com/account/login";
-            Log.d("a","a");
-            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, data,
+            Log.d("a",data.toString());
+            final String url = "https://ota-be-server.herokuapp.com";///account/login
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, data,
                     new Response.Listener<JSONObject>() {
                         @Override
                         public void onResponse(JSONObject response) {
-                            Log.d("b","b");
-                            try {
-                                String ID = response.getString("ID");
-                                String password = response.getString("password");
-                                Toast.makeText(MainActivity.this, password, Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(MainActivity.this, test.class));
-                            }catch(JSONException e) {
-                                e.printStackTrace();
+                                try {
+                                    Log.d("b",response.toString());
+                                    String message=response.getString("message");
+                                    Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                                    if(message == "Login successfully") { // moi sua
+                                        String ID = response.getString("ID");
+                                        Account.account.setID(ID);
+                                        String password = response.getString("password");
+                                        Toast.makeText(MainActivity.this, Account.account.getID(), Toast.LENGTH_SHORT).show();
+                                        startActivity(new Intent(MainActivity.this, test.class));
+                                    }
+                                }catch(JSONException e) {
+                                    e.printStackTrace();
+                                }
                             }
-                        }
+                        //}
                     }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
-                    Log.d("c","c");
-                    Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                    String message=error.toString();
+                    Log.d("c",message);
+                    //Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
                 }
             });
             VolleySingleton.getInstance(this).getRequestQueue().add(jsonObjectRequest);
