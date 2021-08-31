582225a0ec52d04887f3fe84e06c945913b14468
DNX2461 <david.quoccuong2001@gmail.com>
2021-08-31 10:40:18 +0700

    test

--- app/src/main/java/com/example/ota/TeacherActivity.java
+++ app/src/main/java/com/example/ota/TeacherActivity.java
@@ -1,6 +1,9 @@
 package com.example.ota;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,10 @@ public class TeacherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher);
+    }
+
+    public void onClickAttendance(View view){
+        Toast.makeText(this, "Attendance successfully", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(TeacherActivity.this, AttendanceActivity.class));
     }
 }
