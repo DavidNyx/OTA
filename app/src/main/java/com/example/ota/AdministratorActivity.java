9ede9579eacb9709a71643d1585899ed6d428a0c
DNX2461 <david.quoccuong2001@gmail.com>
2021-08-31 11:29:54 +0700

    menu

--- app/src/main/java/com/example/ota/AdministratorActivity.java
+++ app/src/main/java/com/example/ota/AdministratorActivity.java
@@ -1,14 +1,43 @@
 package com.example.ota;
 
-import android.os.Bundle;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AdministratorActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+public class AdministratorActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adminsitrator);
+        getSupportActionBar().setTitle("Administrator Menu");
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#e600ff")));
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.setting:
+                break;
+            case R.id.account:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickLogout(View view){
+        Toast.makeText(this, "Log out successfully", Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(AdministratorActivity.this, MainActivity.class));
     }
 }
