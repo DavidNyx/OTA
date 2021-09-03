2f19d2c35d71735c05f438c3615951084cdea7ec
DNX2461 <david.quoccuong2001@gmail.com>
2021-09-03 16:11:18 +0700

    Update Email.java

--- app/src/main/java/com/example/ota/Email.java
+++ app/src/main/java/com/example/ota/Email.java
@@ -35,41 +35,49 @@ public class Email extends AppCompatActivity {
         EditText editEmail = findViewById(R.id.editEmail);
         EditText editSubject = findViewById(R.id.editSubject);
         EditText editMessage = findViewById(R.id.editMessage);
-        JSONObject data = null;
-        data = new JSONObject();
-        try { // day la data truyen cho server
-            data.put(Account.account.getHeader(),Account.account.getID());
-            data.put("teacherID",editEmail.getText().toString());
-            data.put("topic",editSubject.getText().toString());
-            data.put("content",editMessage.getText().toString());
-            Log.d("d",data.toString());
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        final String url = Account.account.getURL() + "absent-letter/";
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, data,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            Log.d("b",response.toString());
-                            String message=response.getString("message");
-                            Toast.makeText(Email.this, message, Toast.LENGTH_SHORT).show();
-
-                        }catch(JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    //}
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                String message=error.toString();
-                Log.d("c",message);
-                Toast.makeText(Email.this, message, Toast.LENGTH_SHORT).show();
+        JSONObject data = new JSONObject();
+        System.out.println(editEmail.getText().toString());
+        if(editEmail.getText().toString().isEmpty())
+            Toast.makeText(Email.this, "Email is empty. Please enter email to continue.", Toast.LENGTH_SHORT).show();
+        else if(editSubject.getText().toString().isEmpty())
+            Toast.makeText(Email.this, "Topic is empty. Please enter topic to continue.", Toast.LENGTH_SHORT).show();
+        else if(editMessage.getText().toString().isEmpty())
+            Toast.makeText(Email.this, "Message is empty. Please enter message to continue.", Toast.LENGTH_SHORT).show();
+        else {
+            try { // day la data truyen cho server
+                data.put(Account.account.getHeader(), Account.account.getID());
+                data.put("teacherID", editEmail.getText().toString());
+                data.put("topic", editSubject.getText().toString());
+                data.put("content", editMessage.getText().toString());
+                Log.d("d", data.toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-        });
-        VolleySingleton.getInstance(this).getRequestQueue().add(jsonObjectRequest);
+            final String url = Account.account.getURL() + "absent-letter/";
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, data,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                Log.d("b", response.toString());
+                                String message = response.getString("message");
+                                Toast.makeText(Email.this, message, Toast.LENGTH_SHORT).show();
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        //}
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    String message = error.toString();
+                    Log.d("c", message);
+                    Toast.makeText(Email.this, message, Toast.LENGTH_SHORT).show();
+                }
+            });
+            VolleySingleton.getInstance(this).getRequestQueue().add(jsonObjectRequest);
+        }
     }
     private void sendEmail(String Email,String Subject,String Message){
         Notification notification=new NotificationCompat.Builder(this,Channel.CHANNEL_ID)
